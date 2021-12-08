2c1e5df776ff81c5ec828503d7a28f781c28924d
NicolasFares <52455714+NicolasFares@users.noreply.github.com>
2021-12-08 01:24:27 +0100

    BBox detection

--- src/models/BackgroundSubstraction/compare_models.py
+++ src/models/BackgroundSubstraction/compare_models.py
@@ -11,14 +11,34 @@ def process_probs(probs):
     return mask
 
 
+def extract_bbox(frame, mask, threshold):
+    hit = mask.sum(axis=0) / 255 >= threshold
+
+    hit_indexes = np.argwhere(hit != 0)
+    hit = np.array([hit] * frame.shape[0], dtype=np.uint8) * 255
+    if len(hit_indexes) == 0:
+        return frame, hit
+
+    start_box = min(hit_indexes)[0]
+    end_box = max(hit_indexes)[0]
+    if end_box - start_box < 268:
+        return frame, hit
+
+    p1 = (start_box, 0)
+    p2 = (end_box, frame.shape[1])
+    frame = cv2.rectangle(frame, p1, p2, (255, 31, 42), 4)
+
+    return frame, hit
+
+
 if __name__ == "__main__":
     path = f"{DATASET_ROOT}/raw2/images/CV2021_GROUP11/group11.mp4"
     print(path)
     cap = open_or_exit(path)
 
-    frame_scale = 0.66
+    frame_scale = 0.5
     thresh = 1E-50
-    memory_size = 10
+    memory_size = 15
     num_workers = 8
 
     _, frame = cap.read()
@@ -35,7 +55,7 @@ if __name__ == "__main__":
     # Reset video head
     cap.set(2, 0.0)
 
-    frame_rate = FrameRate(verbose=True, frame_rate=250)
+    frame_rate = FrameRate(verbose=True, frame_rate=10)
     frame_counter = 0
     while cap.isOpened():
 
@@ -47,17 +67,19 @@ if __name__ == "__main__":
             GDE_mask = remove_noise(process_probs(GDE(img)), (10, 10), 10)
 
             frame_rate.update()
-            cv2.imshow("Original frame", img)
+            # cv2.imshow("Original frame", img)
             # cv2.imshow("GMM Mask", GMM_mask)
+
+            frame, hit = extract_bbox(img, GDE_mask, 30)
+            cv2.imshow("Frame", frame)
+            cv2.imshow("Hit", hit)
             cv2.imshow("GDE Mask", GDE_mask)
 
-            key = cv2.waitKey(1)
+            key = cv2.waitKey(5)
             if key == ord('q'):
                 break
             elif key == ord('p'):
                 cv2.waitKey(-1)
-
-            break
 
         else:
             break
